2dad0b1c508f4d90bcec96a9f4b9f4db6f3852e5
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2019-12-10 12:23:43 -0800

    Autosave commit created on Tue Dec 10 12:23:43 PST 2019.

--- iggtools/subcommands/__init__.py
+++ iggtools/subcommands/__init__.py
@@ -1,2 +1,2 @@
 # This helps pylint find all subcommands.
-__all__ = ["aws_batch_init", "aws_batch_submit", "init", "build_pangenome", "example_subcommand"]
+__all__ = ["aws_batch_init", "aws_batch_submit", "init", "build_pangenome", "import_uhgg", "example_subcommand"]
