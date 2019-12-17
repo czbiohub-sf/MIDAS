f776f46379a765245d8cae06a7f1c7a596288fa8
Chunyu Zhao <zhaocy.dut@gmail.com>
2019-12-17 11:06:31 -0800

    Autosave commit created on Tue Dec 17 11:06:31 PST 2019.

--- iggtools/subcommands/build_marker_genes.py
+++ iggtools/subcommands/build_marker_genes.py
@@ -235,7 +235,7 @@ def build_marker_genes_slave(args):
     local_gene_files = f"{genome_id}.fna"
     assert os.path.isfile(local_gene_files), f"Failed download annotated marker DNA sequences"
     genes = parse_fasta(local_gene_files)
-    print(genes)
+    print(genes.keys())
 
     # Parse local hmmsearch file
     hmmsearch_seq = f"{genome_id}.markers.fa"
