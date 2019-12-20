874b0e064ff3db22a8451eef43c4ff36716acf7c
Chunyu Zhao <zhaocy.dut@gmail.com>
2019-12-20 14:35:38 -0800

    collate marker gene sequences for rep genomes (#24)

--- iggtools/__main__.py
+++ iggtools/__main__.py
@@ -9,7 +9,7 @@
 # with their own set of command line arguments and subcommand help text -- aside from
 # the shared arguments and shared help text defined in iggtools.common.argparser.
 #
-from iggtools.subcommands import aws_batch_init, aws_batch_submit, init, build_pangenome, import_uhgg, annotate_genes, build_marker_genes  # pylint: disable=unused-import
+from iggtools.subcommands import aws_batch_init, aws_batch_submit, init, build_pangenome, import_uhgg, annotate_genes, build_marker_genes, collate_repgenome_markers  # pylint: disable=unused-import
 from iggtools.common.argparser import parse_args
 
 
