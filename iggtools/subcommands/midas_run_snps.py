a6154a19aa3a98ee55dc6816f9f977bb9a420e83
Chunyu Zhao <chunyu@Chunyus-iMac.local>
2020-01-10 18:48:10 -0800

    Autosave commit created on Fri Jan 10 18:48:10 PST 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -354,7 +354,8 @@ def write_snps_summary(species_alnstats, outfile):
         for species_id, species_aln in species_alnstats.items():
             values = list(species_aln.values())
             tsprint(values)
-            file.write('\t'.join(map(str, [[species_id], values])))
+            values.insert(0, species_id)
+            file.write('\t'.join(map(str, values)))
             ## moved the DECIMALS to the calculation of the values
 
 
