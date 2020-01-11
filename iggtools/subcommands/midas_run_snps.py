be747c137aa04e69c545dbcbdd41c33535d6bc83
Chunyu Zhao <chunyu@Chunyus-iMac.local>
2020-01-10 18:33:22 -0800

    Autosave commit created on Fri Jan 10 18:33:22 PST 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -354,7 +354,7 @@ def write_snps_summary(species_alnstats, outfile):
         for species_id, species_aln in species_alnstats.items():
             tsprint(f"{species_aln.values()}")
             ## to make sure the dict key orders are preserved
-            file.write('\t'.join([species_id, map(str, species_aln.values())]))
+            file.write('\t'.join([species_id, map(str, list(species_aln.values()))]))
             ## moved the DECIMALS to the calculation of the values
 
 
