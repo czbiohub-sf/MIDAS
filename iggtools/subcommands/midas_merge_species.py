3e0cdf24c409e986ca15eb80cb531f19fde61948
Chunyu Zhao <chunyu@Chunyus-iMac.local>
2020-01-27 16:25:08 -0800

    add midas merge species

--- iggtools/subcommands/midas_merge_species.py
+++ iggtools/subcommands/midas_merge_species.py
@@ -1,20 +1,16 @@
 import json
-import random
 import os
 from collections import defaultdict
-from itertools import chain
 import numpy as np
 
 from iggtools.common.argparser import add_subcommand
-from iggtools.common.utils import tsprint, num_physical_cores, command, InputStream, OutputStream, select_from_tsv, multithreading_map, download_reference, TimedSection
-from iggtools.models.uhgg import UHGG
-from iggtools import params
-
+from iggtools.common.utils import tsprint, command, InputStream, OutputStream, select_from_tsv
 
 DEFAULT_SAMPLE_DEPTH = 1.0
 
+DECIMALS = ".3f"
 
-species_abundance_schema = {
+species_profile_schema = {
     "species_id": str,
     "count_reads": int,
     "coverage": float,
@@ -60,9 +56,10 @@ def register_args(main_func):
     return main_func
 
 
-def identify_samples(sample_list):
+def read_samples(sample_list):
     with InputStream(sample_list) as stream:
         samples = dict(select_from_tsv(stream, selected_columns=["sample_name", "midas_output_path"]))
+
     for sample_name in samples.keys():
         midas_output_path = samples[sample_name]
         assert os.path.exists(midas_output_path), f"MIDAS output directory {midas_output_path} for sample {sample_name} not exist."
@@ -72,87 +69,98 @@ def identify_samples(sample_list):
     return samples
 
 
-def read_abundance(species_profile_dir):
-    "Return map of species_id to coverage for the species present in the sample."
-    with InputStream(f"{species_profile_dir}") as stream:
-        return dict(select_from_tsv(stream, selected_columns=species_abundance_schema))
+def read_species_profile(species_profile):
+    with InputStream(species_profile) as instream:
+        for record in select_from_tsv(instream, selected_columns=species_profile_schema, result_structure=dict):
+            yield record
 
 
-def store_data(args, samples, species_ids):
-    # Initialize data of dict(species_id) = dict()
-    data = []
+def transpose(samples, columns):
+    """
+    Collate given columns across samples and transpose the matrix by species_id
+    samples: dict of sample_name: path_to_table_by_sample
+    """
+    transposed = defaultdict(dict)
+    sample_counts = len(samples)
+
+    for sample_index, species_profile in enumerate(samples.values()):
+        for record in read_species_profile(species_profile):
+            species_id = record["species_id"]
+            for col in columns:
+                acc = transposed[col].get(species_id)
+                if not acc:
+                    acc = [species_id] + [0.0] * sample_counts
+                    transposed[col][species_id] = acc
+                acc[1 + sample_index] = record[col]
+    return transposed
+
+
+def cal_prevalence(rowvector, threshold):
+    return sum(1 if val >= threshold else 0 for val in rowvector)
+
+
+def compute_stats(tabundance, tcoverage, args):
+
+    assert tabundance.keys() == tcoverage.keys(), f"Transposed abundance matrix and coverage matrix have different species ids"
+    species_ids = tabundance.keys()
+
+    stats = defaultdict()
     for species_id in species_ids:
-        data[species_id] = {}
-        for field in ['relative_abundance', 'coverage', 'count_reads']:
-            data[species_id][field] = []
-
-    # I think this one liner should be able to
-    data = defaultdict(lambda: defaultdict(list))
-
-    # Read species_profile results for ALL specified samples for ALL the species
-    for sample_name, species_profile in samples.items():
-        # samples.parse_species_profile()
-        abundance = read_abundance(species_profile)
-        for species_id, values in abundance.items():
-            for field in ['relative_abundance', 'coverage', 'count_reads']:
-                if field in values:
-                    data[species_id][field].append(values[field])
-    # data[species_id] has three separate tablles
-    # my question is: how do we keep track of the samples information
-    return data
-
-
-def prevalence(x, y):
-    return sum(1 if rc >= y else 0 for rc in x)
-
-
-def compute_stats(args, data):
-    species_ids = data.keys()
-    stats = dict((species_id, {}) for species_id in species_ids)
-
-    for species_id in species_ids:
-        # abundance
-        x = data[species_id]['relative_abundance']
-        stats[species_id]["median_abundance"] = np.median(x)
-        stats[species_id]["mean_abundance"] = np.mean(x)
-        # coverage
-        x = data[species_id]['coverage']
-        stats[species_id]["median_coverage"] = np.median(x)
-        stats[species_id]["mean_coverage"] = np.mean(x)
-        # prevalence
-        stats[species_id]["prevalence"] = prevalence(x, args.sample_depth) #min_cov
+        species_abun = tabundance[species_id][1:]
+        species_cov = tcoverage[species_id][1:]
+        values = {
+            "species_id": species_id,
+            "median_abundance": np.median(species_abun),
+            "mean_abundance": np.mean(species_abun),
+            "median_coverage": np.median(species_cov),
+            "mean_coverage": np.mean(species_cov),
+            "prevalence": cal_prevalence(species_cov, args.sample_depth)
+        }
+        stats[species_id] = values
     return stats
 
 
+def write_results(samples, transposed, stats, outdir, sort_by="median_coverage"):
+    "Write the transposed tables into separate files"
 
-def write_abundance(args, samples, data):
-    for field in ['relative_abundance', 'coverage', 'count_reads']:
-        with OutputStream(f"{args.outdir}/{field}.txt") as outfile:
-            outfile.write("\t".join(["species_id"] + list(samples.keys())) + "\n")
-            for species_id in data:
-                outfile.write(species_id)
-                for x in data[species_id][field]:
-                    outfile.write("\t%s" % str(x))
-                outfile.write("\n")
-    ## STOP here: the sample information is coded in the order of the data[species_id]['counts'] as the same order of the sample_names.
-    ## todo: what if species_a doesn't exist in sample_b ?? !!
+    header = ["species_id"] + list(samples.keys())
+    for field in list(species_profile_schema.keys())[1:]:
+        path = f"{outdir}/{field}.tsv"
+        data = transposed[field]
+        with OutputStream(path) as outfile:
+            outfile.write("\t".join(header) + "\n")
+            for values in data.values():
+                outfile.write("\t".join(map(str, values)) + "\n")
+
+    # Order species in stats by descending relative abundance
+    outfile = f"{outdir}/species_prevalence.tsv"
+    with OutputStream(outfile) as ostream:
+        header = ["species_id", "median_abundance", "mean_abundance", "median_coverage", "mean_coverage", "prevalence"]
+        ostream.write("\t".join(header) + "\n")
+
+        sorted_species = sorted(((row[sort_by], row["species_id"]) for row in stats.values()), reverse=True)
+        for spec_tuple in sorted_species:
+            species_id = spec_tuple[1]
+            values = map(lambda x: format(x, DECIMALS) if isinstance(x, float) else str(x), list(stats[species_id].values()))
+            ostream.write("\t".join(values) + "\n")
+
 
 def midas_merge_species(args):
+
+    outdir = f"{args.outdir}/merged/species"
+    command(f"rm -rf {outdir}")
+    command(f"mkdir -p {outdir}")
+
     # List samples and species
+    samples = read_samples(args.sample_list)
 
-    #sample_dirs = decode_indirs_arg(args)
-    samples = identify_samples(args.sample_list)
+    # Slice the across-samples species profile matrix by species_id
+    transposed = transpose(samples, list(species_profile_schema.keys())[1:])
 
-    db = UHGG()
-    species_info = db.species
+    # Calculate summary statistics for coverage and relative abundance
+    stats = compute_stats(transposed["relative_abundance"], transposed["coverage"], args)
 
-    # Read in data and computate stats
-    for sample_name, species_profile in samples.items():
-        with InputStream(species_profile) as instream:
-            select_from_tsv(instream, selected_columns=species_abundance_schema)
-
-
-
+    write_results(samples, transposed, stats, outdir, "median_coverage")
 
 
 
