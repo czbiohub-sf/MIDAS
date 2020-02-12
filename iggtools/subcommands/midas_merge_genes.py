481b65594ccef91a37e7cdda4f244bfe930756f5
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-02-12 15:48:40 -0800

    Autosave commit created on Wed Feb 12 15:48:40 PST 2020.

--- iggtools/subcommands/midas_merge_genes.py
+++ iggtools/subcommands/midas_merge_genes.py
@@ -7,7 +7,7 @@ from iggtools.common.utils import tsprint, command, InputStream, OutputStream, s
 from iggtools.params import outputs
 
 DEFAULT_SAMPLE_DEPTH = 1.0
-DEFAULT_SAMPLE_COUNTS = 2
+DEFAULT_SAMPLE_COUNTS = 1
 DEFAULT_CLUSTER_ID = '95'
 DEFAULT_MIN_COPY = 0.35
 
@@ -103,32 +103,13 @@ def register_args(main_func):
     return main_func
 
 
-def check_outdir(args):
-    outdir = f"{args.outdir}/merged/genes"
-    if args.debug and os.path.exists(outdir):
-        tsprint(f"INFO:  Reusing existing output data in {outdir} according to --debug flag.")
-    else:
-        command(f"rm -rf {outdir}")
-        command(f"mkdir -p {outdir}")
-
-    paramstr = f"sd{args.site_depth}"
-    tempdir = f"{outdir}/temp_{paramstr}"
-    if args.debug and os.path.exists(tempdir):
-        tsprint(f"INFO:  Reusing existing temp intermediate data in {tempdir} according to --debug flag.")
-    else:
-        command(f"rm -rf {tempdir}")
-        command(f"mkdir -p {tempdir}")
-
-    return (outdir, tempdir)
-
-
 class Sample:
     """
     base class for sample, to keep track of the midas species/snps/genes path
     """
-    def __init__(self, midas_outdir, sample_name, dbtype):
-        self.dir = midas_outdir
-        self.id = sample_name
+    def __init__(self, dir, id, dbtype):
+        self.dir = dir
+        self.id = id
         self.info = self.read_info(dbtype)
 
     def read_info(self, dbtype):
@@ -154,13 +135,32 @@ class Species:
     """
     Base class for species
     """
-    def __init__(self, species_id):
-        self.species_id = species_id
+    def __init__(self, id):
+        self.id = id
         self.samples = []
         self.sample_depth = []
 
     def fetch_sample_depth(self):
-        self.sample_depth = [sample.info[self.species_id]["mean_coverage"] for sample in self.samples]
+        self.sample_depth = [sample.info[self.id]["mean_coverage"] for sample in self.samples]
+
+
+def check_outdir(args):
+    outdir = f"{args.outdir}/merged/genes"
+    if args.debug and os.path.exists(outdir):
+        tsprint(f"INFO:  Reusing existing output data in {outdir} according to --debug flag.")
+    else:
+        command(f"rm -rf {outdir}")
+        command(f"mkdir -p {outdir}")
+
+    paramstr = f"sd{args.sample_depth}.mc{args.min_copy}.cpid{args.cluster_pid}"
+    tempdir = f"{outdir}/temp_{paramstr}"
+    if args.debug and os.path.exists(tempdir):
+        tsprint(f"INFO:  Reusing existing temp intermediate data in {tempdir} according to --debug flag.")
+    else:
+        command(f"rm -rf {tempdir}")
+        command(f"mkdir -p {tempdir}")
+
+    return (outdir, tempdir)
 
 
 def init_samples(sample_list, dbtype):
@@ -171,7 +171,7 @@ def init_samples(sample_list, dbtype):
     """
     samples = []
     with InputStream(sample_list) as stream:
-        for row in select_from_tsv(stream, selected_columns=["sample_name", "midas_output_path"]):
+        for row in select_from_tsv(stream, selected_columns=["sample_name", "midas_output_path"], result_structure=dict):
             sample = Sample(row["midas_output_path"], row["sample_name"], dbtype)
             samples.append(sample)
     return samples
@@ -210,7 +210,6 @@ def sort_species(species):
     Sort species by sample_counts in descending order
     Output: list of sorted Species objects
     """
-
     species_sorted = sorted(((sp, len(sp.samples)) for sp in species), key=lambda x: x[1], reverse=True)
 
     return [sp[0] for sp in species_sorted]
@@ -228,14 +227,14 @@ def filter_species(species, args):
         # Pick subset of species to analyze
         sp.sample_counts = len(sp.samples)
 
-        if sp.sample_counts < args.min_samples:
+        if sp.sample_counts < args.sample_counts:
             continue # skip low prevalent species
 
         sp.fetch_sample_depth()
         keep.append(sp)
 
         for sample in sp.samples:
-            species_genes_summary.append([sp.species_id, sample.id] + list(sample.info[sp.species_id].values())[1:])
+            species_genes_summary.append([sp.id, sample.id] + list(sample.info[sp.id].values())[1:])
 
     return (keep, species_genes_summary)
 
@@ -285,7 +284,7 @@ def build_gene_matrices(sp, min_copy):
         for field, dtype in genes_info_schema.items():
             sample.genes[field] = defaultdict(dtype)
 
-        path = f"{sample.dir}/genes/output/{sp.species_id}.genes.lz4"
+        path = f"{sample.dir}/genes/output/{sp.id}.genes.lz4"
         for r in read_genes(path):
             gene_id = sp.map[r["gene_id"]]
             sample.genes["copynum"][gene_id] += float(r['copy_number'])
@@ -329,22 +328,30 @@ def collect(accumulator, ps_args):
         acc_copynum = accumulator["copynum"].get(gene_id)
         if not acc_copynum:
             acc_copynum = [0.0] * total_sample_counts
+            accumulator["copynum"][gene_id] = acc_copynum
         acc_copynum[sample_index] += r["copy_number"]
+
 
         acc_depth = accumulator["depth"].get(gene_id)
         if not acc_depth:
             acc_depth = [0.0] * total_sample_counts
+            accumulator["depth"][gene_id] = acc_depth
         acc_depth[sample_index] += r["coverage"]
 
         acc_reads = accumulator["reads"].get(gene_id)
         if not acc_reads:
             acc_reads = [0.0] * total_sample_counts
+            accumulator["reads"][gene_id] = acc_reads
         acc_reads[sample_index] += r["count_reads"]
 
 
-def write(accumulator, sample_names, outdir):
+def write(accumulator, sample_names, outdir, species_id):
+    species_outdir = f"{outdir}/{species_id}"
+    command(f"rm -rf {species_outdir}")
+    command(f"mkdir -p {species_outdir}")
+
     for file_type in genes_info_schema.keys():
-        outfile = f"{outdir}/genes_{file_type}.tsv.lz4"
+        outfile = f"{species_outdir}/genes_{file_type}.tsv.lz4"
         with OutputStream(outfile) as stream:
             stream.write("\t".join(["gene_id"] + sample_names) + "\n")
             for gene_id, gene_vals in accumulator[file_type].items():
@@ -356,7 +363,7 @@ def per_species_worker(arguments):
     species_id, genes_info_path, species_samples, args, outdir = arguments
     sample_names = list(species_samples.keys())
 
-    genes_map = read_cluster_map(genes_info_path, args.cluster_p288id)
+    genes_map = read_cluster_map(genes_info_path, args.cluster_pid)
 
     accumulator = defaultdict(dict)
     for sample_index, sample_name in enumerate(sample_names):
@@ -369,7 +376,7 @@ def per_species_worker(arguments):
     for gene_id, copynum in accumulator["copynum"].items():
         accumulator["presabs"][gene_id] = [1 if cn > args.min_copy else 0 for cn in copynum]
 
-    write(accumulator, sample_names, outdir)
+    write(accumulator, sample_names, outdir, species_id)
 
 
 def midas_merge_genes(args):
@@ -381,12 +388,12 @@ def midas_merge_genes(args):
     # Download gene_info for every species in the restricted species profile.
     def download_genes_info(species_id):
         return download_reference(pangenome_file(species_id, "gene_info.txt.lz4"), f"{tempdir}/{species_id}")
-    genes_info_files = multithreading_hashmap(download_genes_info, [sp.speices_id for sp in species_list], num_threads=20)
+    genes_info_files = multithreading_hashmap(download_genes_info, [sp.id for sp in species_list], num_threads=20)
 
     # Collect copy_number, coverage and read counts across ALl the samples
     argument_list = []
     for sp in species_list:
-        species_id = sp.species_id
+        species_id = sp.id
         species_samples = {sample.id: sample.dir for sample in sp.samples}
         argument_list.append((species_id, genes_info_files[species_id], species_samples, args, outdir))
 
