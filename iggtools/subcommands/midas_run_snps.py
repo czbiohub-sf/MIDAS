0a0edb22501d79474f3ed53f8fabd85f4a26faf3
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-05 15:44:24 -0800

    Autosave commit created on Thu Mar  5 15:44:24 PST 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -278,7 +278,7 @@ def slice_pileup(packed_args):
             ref_allele = contig["contig_seq"][ref_pos]
             print(ref_pos, ref_allele, contig_start, contig_end)
             for nt  in range(4):
-                print("\t".join(map(str, counts[nt])) +  "\n")
+                print("\t".join(map(str, len(counts[nt]))) +  "\n")
             depth = sum([counts[nt][ref_pos] for nt in range(4)])
             count_a = counts[0][ref_pos]
             count_c = counts[1][ref_pos]
