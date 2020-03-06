5f9d5d70b2a4b77353aec59a446408172e6487a3
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-05 16:02:48 -0800

    Autosave commit created on Thu Mar  5 16:02:48 PST 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -344,11 +344,11 @@ def species_pileup(species_ids, contigs_files, repgenome_bamfile):
                     headerless_sliced_path = sample.get_target_layout("contigs_pileup", species_id, slice_id)
 
                     if ni == chunk_num:
-                        slice_args = (species_id, slice_id, contig_id, ci, contig_length, repgenome_bamfile, headerless_sliced_path)
-                        print(species_id, slice_id, contig_id, ci, contig_length)
+                        slice_args = (species_id, slice_id, contig_id, ci+1, contig_length, repgenome_bamfile, headerless_sliced_path)
+                        print(species_id, slice_id, contig_id, ci+1, contig_length)
                     else:
-                        slice_args = (species_id, slice_id, contig_id, ci, ci+slice_size, repgenome_bamfile, headerless_sliced_path)
-                        print(species_id, slice_id, contig_id, ci, ci+slice_size)
+                        slice_args = (species_id, slice_id, contig_id, ci+1, ci+slice_size, repgenome_bamfile, headerless_sliced_path)
+                        print(species_id, slice_id, contig_id, ci+1, ci+slice_size)
 
                     argument_list.append(slice_args)
                     species_sliced_snps_path[species_id].append(headerless_sliced_path)
