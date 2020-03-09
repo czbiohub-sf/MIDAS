f51e47e5077c6e889a21f7e4fa13a95448d05d06
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-09 16:30:24 -0700

    Autosave commit created on Mon Mar  9 16:30:24 PDT 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -285,7 +285,8 @@ def contig_pileup(packed_args):
             for row in records:
                 stream.write("\t".join(map(format_data, row)) + "\n")
 
-        return aln_stats
+        print("aln_stats: {aln_stats}")
+        #return aln_stats
     finally:
         semaphore_for_species[species_id].release() # no deadlock
 
