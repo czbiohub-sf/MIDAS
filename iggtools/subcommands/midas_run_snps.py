50d7f2c79b98ba728732c5e898eb2d890503b2b8
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-10 11:55:25 -0700

    Autosave commit created on Tue Mar 10 11:55:25 PDT 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -367,11 +367,11 @@ def compute_species_pileup_summary(contigs_pileup_summary):
             species_pileup_summary[species_id] = per_species_pileup_stats
 
         perspecies_pileup = species_pileup_summary.get(species_id)
-        perspecies_pileup["genome_length"] +=  record["species_id"]["slice_length"]
-        perspecies_pileup["total_depth"] += record["species_id"]["contig_total_depth"]
-        perspecies_pileup["covered_bases"] += record["species_id"]["contig_covered_bases"]
-        perspecies_pileup["aligned_reads"] += record["species_id"]["aligned_reads"]
-        perspecies_pileup["mapped_reads"] += record["species_id"]["mapped_reads"]
+        perspecies_pileup["genome_length"] +=  record["slice_length"]
+        perspecies_pileup["total_depth"] += record["contig_total_depth"]
+        perspecies_pileup["covered_bases"] += record["contig_covered_bases"]
+        perspecies_pileup["aligned_reads"] += record["aligned_reads"]
+        perspecies_pileup["mapped_reads"] += record["mapped_reads"]
 
         if current_species_id is not None and current_species_id != species_id:
             current_species_pileup = species_pileup_summary.get(current_species_id)
