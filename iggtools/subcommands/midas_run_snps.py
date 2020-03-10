81fb162a2e617f7ccd2449502bab13e8200de259
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-10 15:14:00 -0700

    Autosave commit created on Tue Mar 10 15:14:00 PDT 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -264,9 +264,8 @@ def contig_pileup(packed_args):
                     count_t = counts[3][within_slice_index]
 
                     ref_pos = within_slice_index + contig_start + 1
-                    #print(len(contig["contig_seq"]), ref_pos, contig_start, contig_end, slice_id, contig_id)
-                    #ref_allele = contig["contig_seq"][ref_pos]
-                    ref_allele = ""
+                    print(len(contig["contig_seq"]), ref_pos, contig_start, contig_end, slice_id, contig_id)
+                    ref_allele = contig["contig_seq"][ref_pos]
                     row = (contig_id, ref_pos, ref_allele, depth, count_a, count_c, count_g, count_t)
 
                     aln_stats["contig_total_depth"] += depth
