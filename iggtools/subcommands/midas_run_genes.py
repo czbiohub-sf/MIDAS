398b33354ae3c0a72bb5bf8cc57a65c691452d97
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-13 16:40:34 -0700

    Autosave commit created on Fri Mar 13 16:40:34 PDT 2020.

--- iggtools/subcommands/midas_run_genes.py
+++ iggtools/subcommands/midas_run_genes.py
@@ -309,6 +309,7 @@ def midas_run_genes(args):
         bowtie2_align(bt2_db_dir, bt2_db_name, pangenome_bamfile, args)
         samtools_index(pangenome_bamfile, args.debug)
 
+        species_ids_of_interest = species_ids_of_interest[:1]
         design_chunks(species_ids_of_interest, centroids_files, 1000)
         exit(0)
 
