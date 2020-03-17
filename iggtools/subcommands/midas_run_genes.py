4d9b312ddc19925f4830296e485b98942605b815
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-16 20:36:27 -0700

    Autosave commit created on Mon Mar 16 20:36:27 PDT 2020.

--- iggtools/subcommands/midas_run_genes.py
+++ iggtools/subcommands/midas_run_genes.py
@@ -313,7 +313,8 @@ def midas_run_genes(args):
         build_bowtie2_db(bt2_db_dir, bt2_db_name, centroids_files)
         # Perhaps avoid this giant conglomerated file, fetching instead submaps for each species.
         # TODO: Also colocate/cache/download in master for multiple slave subcommand invocations.
-
+    print(centroids_files)
+    exit(0)
     sample.create_species_subdir(species_ids_of_interest, args.debug, "temp")
 
     # Map reads to pan-genes bowtie2 database
