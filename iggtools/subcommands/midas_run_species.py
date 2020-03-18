7aa747fde811fa5279014a0dda645bfb321920e8
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-18 14:16:28 -0700

    Autosave commit created on Wed Mar 18 14:16:28 PDT 2020.

--- iggtools/subcommands/midas_run_species.py
+++ iggtools/subcommands/midas_run_species.py
@@ -75,7 +75,7 @@ def parse_reads(filename, max_reads=None):
 
 def map_reads_hsblast(m8_file, r1, r2, word_size, markers_db, max_reads):
     assert os.path.exists(os.path.dirname(m8_file)), f"map_reads_hsblast::m8_file: the temp directory doesn't exit {m8_file}."
-
+    assert os.path.exists(markers_db), f"markers db is wrong"
     blast_command = f"hs-blastn align -word_size {word_size} -query /dev/stdin -db {markers_db} -outfmt 6 -num_threads {num_physical_cores} -evalue 1e-3"
     with OutputStream(m8_file, through=blast_command) as blast_input:
         for qid, seq in chain(parse_reads(r1, max_reads), parse_reads(r2, max_reads)):
