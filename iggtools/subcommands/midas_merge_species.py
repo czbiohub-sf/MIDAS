0324aec06f8b57958720239d952ec719ef0f92a0
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-18 21:16:36 -0700

    Autosave commit created on Wed Mar 18 21:16:36 PDT 2020.

--- iggtools/subcommands/midas_merge_species.py
+++ iggtools/subcommands/midas_merge_species.py
@@ -81,6 +81,7 @@ def write_species_results(pool_of_samples, transposed):
         with OutputStream(outpath) as outfile:
             outfile.write("\t".join(["species_id"] + sample_names) + "\n")
             for values in transposed[col].values():
+                print(values)
                 outfile.write("\t".join(map(format_data, values)) + "\n")
 
 
