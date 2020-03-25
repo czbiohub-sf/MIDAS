d2b17a66cca330a7bbaadf723aa24f5069618e00
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-25 09:41:37 -0700

    Autosave commit created on Wed Mar 25 09:41:37 PDT 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -417,8 +417,7 @@ def midas_run_snps(args):
             bt2_species = []
             with InputStream(args.species_profile_path) as stream:
                 for species_id in select_from_tsv(stream, ["species_id"]):
-                    print(species_id)
-                    bt2_species.append(species_id)
+                    bt2_species.append(species_id[0])
             print(bt2_species)
             species_list = list(set(species_list) & set(bt2_species))
             # The index was only for the purpose of same bowtie2 index. but the species_ids_of_interest per sample
