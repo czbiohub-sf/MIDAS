152fe4db310f09baed0656c96ec00139e288dc30
Chunyu Zhao <zhaocy.dut@gmail.com>
2020-03-25 22:08:11 -0700

    Autosave commit created on Wed Mar 25 22:08:11 PDT 2020.

--- iggtools/subcommands/midas_run_snps.py
+++ iggtools/subcommands/midas_run_snps.py
@@ -424,6 +424,7 @@ def midas_run_snps(args):
 
         # Build one bowtie database for species in the restricted species profile
         if bowtie2_index_exists(bt2_db_dir, bt2_db_name):
+            print("we need to build bowtie2 database")
             build_bowtie2_db(bt2_db_dir, bt2_db_name, contigs_files)
         # Perhaps avoid this giant conglomerated file, fetching instead submaps for each species.
         # TODO: Also colocate/cache/download in master for multiple slave subcommand invocations
