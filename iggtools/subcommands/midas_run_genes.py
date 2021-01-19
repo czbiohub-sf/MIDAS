2ad69dfa9732bc11a18b6e1738385f276536ddae
Chunyu Zhao <zhaocy.dut@gmail.com>
2021-01-19 12:26:06 -0800

    temp

--- iggtools/subcommands/midas_run_genes.py
+++ iggtools/subcommands/midas_run_genes.py
@@ -468,7 +468,7 @@ def midas_run_genes(args):
 
         # Build Bowtie indexes for species in the restricted species profile
         tsprint(f"CZ::build_bowtie2_indexes::start")
-        build_bowtie2_db(bt2_db_dir, bt2_db_name, centroids_files, args.num_cores)
+        build_bowtie2_db(bt2_db_dir, bt2_db_name, centroids_files)
         tsprint(f"CZ::build_bowtie2_indexes::finish")
 
         # Align reads to pangenome database
