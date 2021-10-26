7003103218c65659bdd65fa6eae1e9a2d925a093
Ubuntu <ubuntu@ip-172-31-60-111.us-west-2.compute.internal>
2021-10-26 19:19:26 +0000

    scripts to build midas db

---
+++ build_midasdb.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+set -e
+set -x
+
+echo "Build MIDAS-DB locally"
+
+python -m iggtools annotate_genome --species all --midasdb_name testdb --midasdb_dir local_testdb  --debug
+python -m iggtools build_midasdb --generate_gene_feature --genomes all --midasdb_name testdb --midasdb_dir local_testdb --debug
+
+
+python -m iggtools infer_markers --genomes all --midasdb_name testdb --midasdb_dir local_testdb --debug
+python -m iggtools build_midasdb --build_markerdb --midasdb_name testdb --midasdb_dir local_testdb --debug
+
+
+python -m iggtools build_pangenome --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+python -m iggtools build_midasdb --generate_cluster_info --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+
+
+python -m iggtools compute_chunks --chunk_type genes --chunk_size 50000 --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+python -m iggtools compute_chunks --chunk_type run_snps --chunk_size 100000 --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+python -m iggtools compute_chunks --chunk_type merge_snps --chunk_size 500000 --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+
+
+python -m iggtools build_bowtie2db --midasdb_name testdb --midasdb_dir local_testdb --species_list 117086,117088 --bt2_indexes_dir local_testdb/bt2_indexes --bt2_indexes_name repgenomes
+python -m iggtools build_bowtie2db --midasdb_name testdb --midasdb_dir local_testdb --species_list 117086,117088 --bt2_indexes_dir local_testdb/bt2_indexes --bt2_indexes_name pangenomes
