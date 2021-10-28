25eb82972a74cb1c04406df2106bf84a8e1066b9
Ubuntu <ubuntu@ip-172-31-60-111.us-west-2.compute.internal>
2021-10-28 00:53:29 +0000

    build local test midas db

--- tests/build_midasdb.sh
+++ tests/build_midasdb.sh
@@ -2,28 +2,33 @@
 set -e
 set -x
 
-echo "Build MIDAS-DB locally"
+basedir=`pwd`
+testdir="${basedir}/tests"
+echo ${testdir}
 
-python -m iggtools annotate_genome --species all --midasdb_name testdb --midasdb_dir local_testdb  --debug
-python -m iggtools build_midasdb --generate_gene_feature --genomes all --midasdb_name testdb --midasdb_dir local_testdb --debug
+db_dir="$testdir/$db_dir/local_testdb"
 
+echo "Build MIDAS-DB locally $db_dir"
 
-python -m iggtools infer_markers --genomes all --midasdb_name testdb --midasdb_dir local_testdb --debug
-python -m iggtools build_midasdb --build_markerdb --midasdb_name testdb --midasdb_dir local_testdb --debug
-
-
-python -m iggtools build_pangenome --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
-python -m iggtools build_midasdb --generate_cluster_info --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+python -m iggtools annotate_genome --species all --midasdb_name testdb --midasdb_dir $db_dir  --debug --force
+python -m iggtools build_midasdb --generate_gene_feature --genomes all --midasdb_name testdb --midasdb_dir $db_dir --debug --force
 
 
-myArray=(50000 100000 500000 1000000 2000000)
+python -m iggtools infer_markers --genomes all --midasdb_name testdb --midasdb_dir $db_dir --debug --force
+python -m iggtools build_midasdb --build_markerdb --midasdb_name testdb --midasdb_dir $db_dir --debug --force
+
+
+python -m iggtools build_pangenome --species all --midasdb_name testdb --midasdb_dir $db_dir --debug --force
+python -m iggtools build_midasdb --generate_cluster_info --species all --midasdb_name testdb --midasdb_dir $db_dir --debug --force
+
+
+myArray=(100000 500000 1000000)
 for chunksize in ${myArray[@]}; do
-  python -m iggtools compute_chunks --chunk_type genes --chunk_size $chunksize --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
-  python -m iggtools compute_chunks --chunk_type run_snps --chunk_size $chunksize --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+  python -m iggtools compute_chunks --chunk_type genes --chunk_size $chunksize --species all --midasdb_name testdb --midasdb_dir $db_dir --debug --force
+  python -m iggtools compute_chunks --chunk_type run_snps --chunk_size $chunksize --species all --midasdb_name testdb --midasdb_dir $db_dir --debug --force
 done
 
-python -m iggtools compute_chunks --chunk_type merge_snps --chunk_size 500000 --species all --midasdb_name testdb --midasdb_dir local_testdb --debug
+python -m iggtools compute_chunks --chunk_type merge_snps --chunk_size 500000 --species all --midasdb_name testdb --midasdb_dir $db_dir --debug --force
 
-
-python -m iggtools build_bowtie2db --midasdb_name testdb --midasdb_dir local_testdb --species_list 117086,117088 --bt2_indexes_dir local_testdb/bt2_indexes --bt2_indexes_name repgenomes
-python -m iggtools build_bowtie2db --midasdb_name testdb --midasdb_dir local_testdb --species_list 117086,117088 --bt2_indexes_dir local_testdb/bt2_indexes --bt2_indexes_name pangenomes
+python -m iggtools build_bowtie2db --midasdb_name testdb --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name repgenomes
+python -m iggtools build_bowtie2db --midasdb_name testdb --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name pangenomes
