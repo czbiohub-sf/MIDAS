81b83b787bae5727f7bb84449f1d50ad3131aa4c
zhaoc1 <zhaocy.dut@gmail.com>
2022-04-07 06:46:30 +0000

    add conda recipe

--- tests/test_db.sh
+++ tests/test_db.sh
@@ -6,9 +6,10 @@ testdir="${basedir}/tests"
 echo ${testdir}
 
 db_name="testdb"
-db_dir="$testdir/$db_dir/midas2_database_test"
+db_dir="$testdir"
 
 echo "Build MIDAS-DB locally $db_dir"
+
 
 python3.7 -m midas2 annotate_genome --species all --midasdb_name $db_name --midasdb_dir $db_dir  --debug --force
 python3.7 -m midas2 build_midasdb --generate_gene_feature --genomes all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
@@ -22,15 +23,15 @@ python3.7 -m midas2 build_pangenome --species all --midasdb_name $db_name --mida
 python3.7 -m midas2 build_midasdb --generate_cluster_info --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
 
 
-myArray=(100000 500000 1000000)
+myArray=(500000 1000000)
 for chunksize in ${myArray[@]}; do
-  python3 -m midas2 compute_chunks --chunk_type genes --chunk_size $chunksize --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
-  python3 -m midas2 compute_chunks --chunk_type run_snps --chunk_size $chunksize --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+  python3.7 -m midas2 compute_chunks --chunk_type genes --chunk_size $chunksize --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+  python3.7 -m midas2 compute_chunks --chunk_type run_snps --chunk_size $chunksize --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
 done
 
-python3 -m midas2 compute_chunks --chunk_type merge_snps --chunk_size 500000 --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+python3.7 -m midas2 compute_chunks --chunk_type merge_snps --chunk_size 500000 --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
 
-python3 -m midas2 build_bowtie2db --midasdb_name $db_name --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name repgenomes
-python3 -m midas2 build_bowtie2db --midasdb_name $db_name --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name pangenomes
+python3.7 -m midas2 build_bowtie2db --midasdb_name $db_name --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name repgenomes
+python3.7 -m midas2 build_bowtie2db --midasdb_name $db_name --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name pangenomes
 
 echo "SUCESS MIDAS2 Database Build"
