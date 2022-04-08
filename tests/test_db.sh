e35b32f5d96553dcdc795e9d18e177186997c386
Chunyu Zhao <zhaocy.dut@gmail.com>
2022-04-08 00:55:55 -0700

    v0.9.3 migrate s3 bucket  (#78)

--- tests/test_db.sh
+++ tests/test_db.sh
@@ -8,30 +8,36 @@ echo ${testdir}
 db_name="testdb"
 db_dir="$testdir"
 
-echo "Build MIDAS-DB locally $db_dir"
+echo "START MIDAS 2.0 Database Build Testing"
+echo "testdb is built locally at $db_dir"
+
+rm -rf $db_dir
 
 
-python3.7 -m midas2 annotate_genome --species all --midasdb_name $db_name --midasdb_dir $db_dir  --debug --force
-python3.7 -m midas2 build_midasdb --generate_gene_feature --genomes all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+echo "Annotate Genomes"
+midas2 annotate_genome --species all --midasdb_name $db_name --midasdb_dir $db_dir  --debug --force
+midas2 build_midasdb --generate_gene_feature --genomes all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+
+echo "Infer SGC genes and Build marker DB"
+midas2 infer_markers --genomes all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+midas2 build_midasdb --build_markerdb --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+
+echo "Build Pangenomes"
+midas2 build_pangenome --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
+midas2 build_midasdb --generate_cluster_info --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
 
 
-python3.7 -m midas2 infer_markers --genomes all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
-python3.7 -m midas2 build_midasdb --build_markerdb --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
-
-
-python3.7 -m midas2 build_pangenome --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
-python3.7 -m midas2 build_midasdb --generate_cluster_info --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
-
-
-myArray=(500000 1000000)
+echo "Compute Chunks"
+myArray=(1000000)
 for chunksize in ${myArray[@]}; do
   python3.7 -m midas2 compute_chunks --chunk_type genes --chunk_size $chunksize --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
   python3.7 -m midas2 compute_chunks --chunk_type run_snps --chunk_size $chunksize --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
 done
-
 python3.7 -m midas2 compute_chunks --chunk_type merge_snps --chunk_size 500000 --species all --midasdb_name $db_name --midasdb_dir $db_dir --debug --force
 
+
+echo "Build Bowtie2 Databases"
 python3.7 -m midas2 build_bowtie2db --midasdb_name $db_name --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name repgenomes
 python3.7 -m midas2 build_bowtie2db --midasdb_name $db_name --midasdb_dir $db_dir --species_list 117086,117088 --bt2_indexes_dir $db_dir/bt2_indexes --bt2_indexes_name pangenomes
 
-echo "SUCESS MIDAS2 Database Build"
+echo "SUCCESS FINISH MIDAS 2.0 Database Build"
