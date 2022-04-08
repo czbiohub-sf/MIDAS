33495e5ebc44a8ddf0354643a401ed51f2ed0683
zhaoc1 <zhaocy.dut@gmail.com>
2022-04-08 07:45:33 +0000

    update test

--- tests/test_analysis.sh
+++ tests/test_analysis.sh
@@ -10,13 +10,16 @@ num_cores=$1
 
 basedir=`pwd`
 testdir="${basedir}/tests"
-echo ${testdir}
 
 outdir="${testdir}/midas2_output"
+rm -rf ${outdir}
 mkdir -p ${outdir}
+
 midas_outdir="${outdir}/single_sample"
 merge_midas_outdir="${outdir}/across_samples"
-midas_db="${outdir}/midasdb_gtdb"
+
+midas_dbname="uhgg"
+midas_db="${outdir}/midasdb_${midas_dbname}"
 
 logs_dir="${outdir}/logs"
 mkdir -p "${logs_dir}"
@@ -29,38 +32,39 @@ rm -rf ${pool_fp}
 
 ls "${testdir}/reads" | awk -F '_' '{print $1}' > ${samples_fp}
 
+echo "MIDAS 2.0 Unit Testing Start"
 
 echo -e "sample_name\tmidas_outdir" > ${pool_fp}
 cat ${samples_fp} | awk -v OFS='\t' -v dir=$midas_outdir '{print $1, dir}' >> ${pool_fp}
 
 
-echo "test run_species"
-cat ${samples_fp} | xargs -Ixx bash -c "midas2 run_species --sample_name xx -1 ${testdir}/reads/xx_R1.fastq.gz --num_cores ${num_cores} --midasdb_name gtdb --midasdb_dir ${midas_db} ${midas_outdir} &> ${logs_dir}/xx_species_${num_cores}.log"
+echo "Testing Single-Sample Species Module"
+cat ${samples_fp} | xargs -Ixx bash -c "midas2 run_species --sample_name xx -1 ${testdir}/reads/xx_R1.fastq.gz --num_cores ${num_cores} --midasdb_name ${midas_dbname} --midasdb_dir ${midas_db} ${midas_outdir} &> ${logs_dir}/xx_species_${num_cores}.log"
 
-echo "test merge_species"
+echo "Testing Across-Samples Species Module"
 midas2 merge_species --samples_list ${pool_fp} --marker_depth 0.5 ${merge_midas_outdir} &> ${logs_dir}/merge_species_${num_cores}.log
 
 
-echo "test build_bowtie2: select species by prevalence"
-midas2 build_bowtie2db --midasdb_name gtdb --midasdb_dir ${midas_db} --species_profile ${merge_midas_outdir}/species/species_prevalence.tsv --select_by sample_counts --select_threshold 1 --num_cores ${num_cores} --bt2_indexes_dir ${merge_midas_outdir}/bt2_indexes &> ${logs_dir}/build_bowtie2_rep_${num_cores}.log
-midas2 build_bowtie2db --midasdb_name gtdb --midasdb_dir ${midas_db} --bt2_indexes_name pangenomes --species_profile ${merge_midas_outdir}/species/species_prevalence.tsv --select_by sample_counts --select_threshold 1 --num_cores ${num_cores} --bt2_indexes_dir ${merge_midas_outdir}/bt2_indexes &> ${logs_dir}/build_bowtie2_pan_${num_cores}.log
+echo "Testing Build Bowtie2 Genome Databases via species prevalence"
+midas2 build_bowtie2db --midasdb_name ${midas_dbname} --midasdb_dir ${midas_db} --species_profile ${merge_midas_outdir}/species/species_prevalence.tsv --select_by sample_counts --select_threshold 1 --num_cores ${num_cores} --bt2_indexes_dir ${merge_midas_outdir}/bt2_indexes &> ${logs_dir}/build_bowtie2_rep_${num_cores}.log
+midas2 build_bowtie2db --midasdb_name ${midas_dbname} --midasdb_dir ${midas_db} --bt2_indexes_name pangenomes --species_profile ${merge_midas_outdir}/species/species_prevalence.tsv --select_by sample_counts --select_threshold 1 --num_cores ${num_cores} --bt2_indexes_dir ${merge_midas_outdir}/bt2_indexes &> ${logs_dir}/build_bowtie2_pan_${num_cores}.log
 
 
-echo "test run_snps with prebuilt bowtie indexes (no need to run species flow)"
-cat ${samples_fp} | xargs -Ixx bash -c "midas2 run_snps --sample_name xx -1 ${testdir}/reads/xx_R1.fastq.gz --num_cores ${num_cores} --chunk_size 500000 --midasdb_name gtdb --midasdb_dir ${midas_db} --select_threshold=-1 \
+echo "Testing Single-Sample SNPs Module with Prebuilt Bowtie2 Genome Databases" # (no need to run species flow)
+cat ${samples_fp} | xargs -Ixx bash -c "midas2 run_snps --sample_name xx -1 ${testdir}/reads/xx_R1.fastq.gz --num_cores ${num_cores} --chunk_size 500000 --midasdb_name ${midas_dbname} --midasdb_dir ${midas_db} --select_threshold=-1 \
   --advanced --prebuilt_bowtie2_indexes ${merge_midas_outdir}/bt2_indexes/repgenomes --prebuilt_bowtie2_species ${merge_midas_outdir}/bt2_indexes/repgenomes.species ${midas_outdir} &> ${logs_dir}/xx_snps_${num_cores}_w_bowtie2.log"
 
 
-echo "test merge_snps"
-midas2 merge_snps --samples_list ${pool_fp} --midasdb_name gtdb --midasdb_dir ${midas_db} --advanced --num_cores ${num_cores} --chunk_size 1000000 --genome_coverage 0.6 ${merge_midas_outdir} &>  ${logs_dir}/merge_snps_${num_cores}.log
+echo "Testing Across-Samples SNPs Module"
+midas2 merge_snps --samples_list ${pool_fp} --midasdb_name ${midas_dbname} --midasdb_dir ${midas_db} --advanced --num_cores ${num_cores} --chunk_size 1000000 --genome_coverage 0.6 ${merge_midas_outdir} &>  ${logs_dir}/merge_snps_${num_cores}.log
 
 
-echo "test run_genes"
-cat ${samples_fp} | xargs -Ixx bash -c "midas2 run_genes --sample_name xx -1 ${testdir}/reads/xx_R1.fastq.gz --num_cores ${num_cores} --midasdb_name gtdb --midasdb_dir ${midas_db} --select_by unique_fraction_covered --select_threshold 0.4 ${midas_outdir}  &> ${logs_dir}/xx_genes_${num_cores}.log"
+echo "Testing Single-Sample Genes Module"
+cat ${samples_fp} | xargs -Ixx bash -c "midas2 run_genes --sample_name xx -1 ${testdir}/reads/xx_R1.fastq.gz --num_cores ${num_cores} --midasdb_name ${midas_dbname} --midasdb_dir ${midas_db} --select_by unique_fraction_covered --select_threshold 0.4 ${midas_outdir}  &> ${logs_dir}/xx_genes_${num_cores}.log"
 
 
-echo "test merge_genes default"
-midas2 merge_genes --samples_list ${pool_fp} --midasdb_name gtdb --midasdb_dir ${midas_db} --num_cores ${num_cores} --sample_counts 2 ${merge_midas_outdir} &> ${logs_dir}/merge_genes_${num_cores}.log
+echo "Testing Across-Samples Genes Module" # (default)
+midas2 merge_genes --samples_list ${pool_fp} --midasdb_name ${midas_dbname} --midasdb_dir ${midas_db} --num_cores ${num_cores} --sample_counts 2 ${merge_midas_outdir} &> ${logs_dir}/merge_genes_${num_cores}.log
 
 
-echo "SUCCESS MIDAS 2.0 Unit Testing"
+echo "MIDAS 2.0 Unit Testing SUCCESS"
