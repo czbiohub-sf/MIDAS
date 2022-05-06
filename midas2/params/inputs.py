f71d2caddf8da13d1d5629626e7006087aae65bc
Chunyu Zhao <zhaocy.dut@gmail.com>
2022-05-06 15:59:45 -0700

    add test https

--- midas2/params/inputs.py
+++ midas2/params/inputs.py
@@ -2,10 +2,13 @@
 # See https://github.com/czbiohub/MIDAS2.0/wiki/MIDAS-DB#inputs
 
 igg = "s3://microbiome-pollardlab"
+gidb = "https://microbiome-pollardlab.s3.us-west-2.amazonaws.com"
+
 MIDASDB_DICT = {
     "uhgg": f"{igg}/uhgg_v1",
     "gtdb": f"{igg}/gtdb_r202",
-    "testdb": f"{igg}/testdb", # <-- dummy db
+    "testdb": f"{igg}/testdb",
+    "gidb": f"{gidb}/gidb"
 }
 MIDASDB_VERSION = {
     "uhgg": f"version 1.0",
@@ -14,7 +17,6 @@ MIDASDB_VERSION = {
 MIDASDB_NAMES = list(MIDASDB_DICT.keys())
 
 
-# Check out https://github.com/czbiohub/MIDAS2.0/wiki/MIDAS-DB#marker-genes-identification
 marker_set = "phyeco"
 MARKER_FILE_EXTS = ["fa", "fa.bwt", "fa.header", "fa.sa", "fa.sequence", "map"]
 hmmsearch_max_evalue = 1e-5
