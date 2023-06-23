40eaba44ab86e9c99b1a71b6bca186dd112a47e2
Chunyu Zhao <czhao@ruby.gladstone.internal>
2023-06-23 00:11:55 -0700

    add recluster

--- midas2/subcommands/__init__.py
+++ midas2/subcommands/__init__.py
@@ -4,4 +4,4 @@ __all__ = ["aws_batch_init", "aws_batch_submit", "init", "database", \
             "build_midasdb", "build_bowtie2db", "compute_chunks", \
             "run_species", "run_genes", "run_snps", \
             "merge_species", "merge_snps", "merge_genes", \
-            "example_subcommand"]
+            "recluster_centroids", "example_subcommand"]
